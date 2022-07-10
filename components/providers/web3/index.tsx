19faf4d6d2a692a123ea14e81989dae45c3d261a
toki suzuki <tokinoshin.suzuki@pilotz.jp>
2022-07-10 23:43:32 +0900

    [Add] web3 state

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,11 +1,12 @@
 import { createContext, FC, useContext, useState } from "react";
+import { createDefaultState, Web3State } from "./utils";
 
-const Web3Context = createContext<any>(null);
+const Web3Context = createContext<Web3State>(createDefaultState());
 interface Props {
   children: React.ReactNode;
 }
 const Web3Provider: FC<Props> = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState({ test: "hello Provider" });
+  const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,20 @@ import type { NextPage } from "next";
 import { BaseLayout, NftList } from "@ui";
 import nfts from "../content/meta.json";
 import { NftMeta } from "@_types/nft";
+import { useWeb3 } from "@providers/web3";
 
 const Home: NextPage = () => {
+  const { ethereum, provider, isLoading, contract } = useWeb3();
+
   return (
     <BaseLayout>
+      {`ethereum: ${ethereum}`}
+      <br />
+      {`provider: ${provider}`}
+      <br />
+      {`isLoading: ${isLoading}`}
+      <br />
+      {`contract: ${contract}`}
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
