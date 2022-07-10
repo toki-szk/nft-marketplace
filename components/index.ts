2283144d510faaf16e9f3f1daa588b1c4d1bda4b
toki suzuki <tokinoshin.suzuki@pilotz.jp>
2022-07-10 22:34:58 +0900

    [fix] front fix

--- components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./navbar";
-export { default as BaseLayout } from "./layout/BaseLayout";
-export { default as NftList } from "./nft/list";
-export { default as NftItem } from "./nft/item";
