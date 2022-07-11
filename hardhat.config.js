806f633a524b0ee65b8e00a960392d19da0cc042
toki suzuki <tokinoshin.suzuki@pilotz.jp>
2022-07-11 11:38:25 +0900

    [Add] nftmarket contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  paths: {
+    cache: "./public/contracts/cache",
+    artifacts: "./public/contracts/artifacts",
+  },
 };
