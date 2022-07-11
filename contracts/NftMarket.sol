92908191244075409ab67e08069631aa70d76563
toki suzuki <tokinoshin.suzuki@pilotz.jp>
2022-07-11 11:52:51 +0900

    [Add] openzeppelin

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-contract NftMarket {
+contract NftMarket is ERC721URIStorage {
+  constructor() ERC721("CreaturesNFT", "CNFT") {}
 }
